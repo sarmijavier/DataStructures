44ce67803ecf8e81178b53bc1073f95fb9c6fc73
Yahuarcani <mayayahuarcani@gmail.com>
2021-01-02 17:31:03 -0500

    arrays and its methods

--- array.js
+++ array.js
@@ -16,6 +16,36 @@ class MyArray {
         this.length++;
         return this.data;
     }
+
+    pop() {
+        delete this.data[this.length - 1];
+        this.length--;
+        return this.data;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+
+        return item;
+    }
+
+    shiftIndex(index) {
+        for(let i = index; i < this.length - 1; i++ ){
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+
+    unshift (item) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+        this.data[0] = item;
+        this.length++;
+        return this.data;
+    }
 }
 
 const myArray = new MyArray();
